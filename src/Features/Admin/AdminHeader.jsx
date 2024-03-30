0af3963a299b2c1a5dec2fcfa0b56d3c8a08ed59
Dandrizzy <dandrizzy4all@gmail.com>
2024-03-30 09:52:07 +0100

    up

--- src/Features/Admin/AdminHeader.jsx
+++ src/Features/Admin/AdminHeader.jsx
@@ -7,7 +7,7 @@ const AdminHeader = () => {
   <div className="flex justify-between items-center p-4 bg-blue-500 text-blue-50">
    <div className=" flex gap-2 items-center">
     <AdminNav />
-    <p>GOLDEN TRADE FX</p>
+    <p>GT FX</p>
    </div>
    <AdminInfo />
   </div>
--- src/ui/Brand.jsx
+++ src/ui/Brand.jsx
@@ -2,7 +2,7 @@
 const Brand = () => {
  return (
   <div className=" text-neutral-800 sm:text-2xl text-xl tracking-widest uppercase flex gap-3 items-center font-bold">
-   <p>Golden trade fx</p>
+   <p>GT FX</p>
   </div>
  );
 };
